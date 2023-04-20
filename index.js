fba173e923109af5cbd8b3a5f60fb628914de289
Nikita Pavlyuk <nikitapavlyuk@Nikitas-Laptop.local>
2023-04-20 20:21:21 +0700

    fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const args = process.argv.slice(2);
 
 const limit = args[0] || 3;
 
-fs.writeFileSync("result.csv", "Text,Buidling,Score\n");
+fs.writeFileSync("result.csv", "Text,Buidling,Scores\n");
 
 const configuration = new Configuration({
   apiKey: process.env.TOKEN,
@@ -17,12 +17,14 @@ const openai = new OpenAIApi(configuration);
 const predict = async (text) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Extract the building name from the text, no response formatting, just simply the name' +
-     '"""' +
-     '${text}'`,
+    prompt: `
+      Identify about which building in Bangkok the following text is about: "${text}".
+      No response formatting. Send only building name.
+    `,
     temperature: 0,
     max_tokens: 100,
     top_p: 1.0,
+    logprobs: 1,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   });
@@ -30,21 +32,31 @@ const predict = async (text) => {
   return {
     prediction: response.data.choices[0].text,
     text,
+    scores: JSON.stringify(
+      response.data.choices[0].logprobs.top_logprobs
+    ).replace(/,/g, " "),
   };
 };
 
 (async () => {
   const results = (
     await Promise.allSettled(texts.slice(0, limit).map((text) => predict(text)))
-  ).filter((item) => item.status === "fulfilled" && item.value);
+  ).filter((item) => {
+    if (item.status === "fulfilled" && item.value) {
+      return true;
+    } else {
+      console.log("Error");
+    }
+  });
 
   results.forEach((item) => {
     fs.appendFileSync(
       "result.csv",
-      `${item.value.text},${item.value.prediction}`.replace(/\n/g, "") + "\n"
+      `${item.value.text},${item.value.prediction},${item.value.scores}`.replace(
+        /\n/g,
+        ""
+      ) + "\n"
     );
-
-    console.log(item);
   });
 
   console.log("Done");
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Run
+
+```
+yarn start <limit>
+```
+
+Default limit is `3`
+
+- raw-data.json - scrapped table from mongodb
+- texts.json - cleared descriptions
